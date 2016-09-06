bf635c96a2791d10d8d4fcacba8e5d9ae27025a3
sharifelgamal <selgamal@google.com>
2016-09-06 16:54:30 -0700

    adding third_party folder for mkimage stuff and the ability to specify which repo debian gets pushed to

--- build.sh
+++ build.sh
@@ -2,8 +2,14 @@
 set -e
 set -x
 TAG=`date +%Y-%m-%d`
-cd mkdebootstrap && gcloud alpha container builds create . -t gcr.io/sharif-test/mkdebootstrap:$TAG
-cd ../docker-mkdebootstrap && gcloud alpha container builds create . -t gcr.io/sharif-test/docker-mkdebootstrap:$TAG
+REPO=$1
+cp -R third_party/docker/* mkdebootstrap/
+cd mkdebootstrap && gcloud alpha container builds create . -t $REPO/mkdebootstrap:jessie
 cd ..
-sed -i 's/\$REPLACEME\$/'$TAG'/' cloudbuild.yaml
+cp -R third_party/docker/* docker-mkdebootstrap/
+cd docker-mkdebootstrap && gcloud alpha container builds create . -t $REPO/docker-mkdebootstrap:jessie
+cd ..
+# need to escape forward slashes in the sed command
+sed -i 's/\$REPO\$/'${REPO//\//\\\/}'/' cloudbuild.yaml
+sed -i 's/\$TAG\$/'$TAG'/' cloudbuild.yaml
 gcloud alpha container builds create . --config=cloudbuild.yaml
--- docker-mkdebootstrap/docker-mkimage.sh
+++ docker-mkdebootstrap/docker-mkimage.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+# Temp step to get around Argo not running docker commands in privileged mode.
+# Will be fixed with b/31267381.
 set -e
 set -x
 apiversion=1.21
 export DOCKER_API_VERSION="$apiversion"
 TAG=`date +%Y-%m-%d`
-docker run --name mkdebootstrap --privileged --volume /workspace gcr.io/sharif-test/mkdebootstrap:$TAG -d /workspace --compression bz2 debootstrap --variant=minbase jessie
+docker run --name mkdebootstrap --privileged --volume /workspace gcr.io/sharif-test/mkdebootstrap:jessie -d /workspace --compression bz2 debootstrap --variant=minbase jessie
 docker cp mkdebootstrap:/workspace/rootfs.tar.bz2 /workspace
 docker rm --volumes mkdebootstrap
