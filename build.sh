60e123158a02407315d42ed5ba1624a99cd6d315
Sharif Elgamal <selgamal@google.com>
2016-09-08 11:19:56 -0700

    Using container builder to build debian image (#20)
    * changing the build process for debian
    
    * adding third_party folder for mkimage stuff and the ability to specify which repo debian gets pushed to
    
    * moving third party stuff to third_party and parameterizing scripts
    
    * cleaning up the build, moving it all the cloudbuild
    
    * cleaning up file copying
    
    * version specific URL for docker binary
    
    * no need to push an intermediate image

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+set -x
+TAG=$(date +%Y-%m-%d)
+REPO=$1
+VERSION=$2
+
+cp -R third_party/docker/mkimage* mkdebootstrap/
+cp -R third_party/docker/docker docker-mkdebootstrap/
+
+sed -i "s|%REPO%|$REPO|g" cloudbuild.yaml
+sed -i "s|%TAG%|$TAG|g" cloudbuild.yaml
+sed -i "s|%VERSION%|$VERSION|g" cloudbuild.yaml
+gcloud alpha container builds create . --config=cloudbuild.yaml
---
+++ docker-mkdebootstrap/docker-mkimage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Container builder doesn't currently run in privileged mode so this script runs
+# docker inside docker to allow privileged mode so debootstrap can run chroot.
+# Once container builder allows privileged mode, this script will go away.
+set -e
+set -x
+apiversion=1.21
+export DOCKER_API_VERSION="$apiversion"
+REPO=$1
+VERSION=$2
+docker run --name mkdebootstrap --privileged --volume /workspace $REPO/mkdebootstrap:jessie -d /workspace --compression bz2 debootstrap --variant=minbase $VERSION
+docker cp mkdebootstrap:/workspace/rootfs.tar.bz2 /workspace
+docker rm --volumes mkdebootstrap
