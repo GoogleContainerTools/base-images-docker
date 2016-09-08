4e628a5cadd4d04def79543647fdab79c03c4161
sharifelgamal <selgamal@google.com>
2016-09-07 17:34:55 -0700

    cleaning up the build, moving it all the cloudbuild

--- build.sh
+++ build.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -e
 set -x
-TAG=`date +%Y-%m-%d`
+TAG=$(date +%Y-%m-%d)
 REPO=$1
+VERSION=$2
+
 cp -R third_party/docker/* mkdebootstrap/
-cd mkdebootstrap && gcloud alpha container builds create . -t $REPO/mkdebootstrap:jessie
-cd ..
 cp -R third_party/docker/* docker-mkdebootstrap/
-cd docker-mkdebootstrap && gcloud alpha container builds create . -t $REPO/docker-mkdebootstrap:jessie
-cd ..
-# need to escape forward slashes in the sed command
-sed -i 's/\$REPO\$/'${REPO//\//\\\/}'/' cloudbuild.yaml
-sed -i 's/\$TAG\$/'$TAG'/' cloudbuild.yaml
+
+sed -i "s|%REPO%|$REPO|g" cloudbuild.yaml
+sed -i "s|%TAG%|$TAG|g" cloudbuild.yaml
+sed -i "s|%VERSION%|$VERSION|g" cloudbuild.yaml
 gcloud alpha container builds create . --config=cloudbuild.yaml
--- docker-mkdebootstrap/docker-mkimage.sh
+++ docker-mkdebootstrap/docker-mkimage.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-# Cloud build doesn't currently run in privileged mode so this script runs
+# Container builder doesn't currently run in privileged mode so this script runs
 # docker inside docker to allow privileged mode so debootstrap can run chroot.
-# Once cloud build allows privileged mode, this script will go away.
+# Once container builder allows privileged mode, this script will go away.
 set -e
 set -x
 apiversion=1.21
 export DOCKER_API_VERSION="$apiversion"
-TAG=`date +%Y-%m-%d`
-docker run --name mkdebootstrap --privileged --volume /workspace gcr.io/sharif-test/mkdebootstrap:jessie -d /workspace --compression bz2 debootstrap --variant=minbase jessie
+REPO=$1
+VERSION=$2
+docker run --name mkdebootstrap --privileged --volume /workspace $REPO/mkdebootstrap:jessie -d /workspace --compression bz2 debootstrap --variant=minbase $VERSION
 docker cp mkdebootstrap:/workspace/rootfs.tar.bz2 /workspace
 docker rm --volumes mkdebootstrap
