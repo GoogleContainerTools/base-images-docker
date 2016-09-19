f589b9753d3c4a95f1cb81f08064b1680a862e36
Sharif Elgamal <selgamal@google.com>
2016-09-19 16:38:44 -0400

    change image name to debian8 for jessie (#27)
    * change image name to debian8 for jessie
    
    * error handling and more comments
    
    * merge conflict

--- build.sh
+++ build.sh
@@ -11,9 +11,18 @@ then
   GCLOUD_CMD=$4
 fi
 
+if [ "$VERSION" == "jessie" ]
+then
+  VERSION_NUMBER=8
+else
+  echo "Invalid version $VERSION"
+  exit 1
+fi
+
 cp -R third_party/docker/mkimage* mkdebootstrap/
 
 sed -i "s|%REPO%|$REPO|g" cloudbuild.yaml
 sed -i "s|%TAG%|$TAG|g" cloudbuild.yaml
 sed -i "s|%VERSION%|$VERSION|g" cloudbuild.yaml
+sed -i "s|%NUM%|$VERSION_NUMBER|g" cloudbuild.yaml
 $GCLOUD_CMD alpha container builds create . --config=cloudbuild.yaml --verbosity=info --gcs-source-staging-dir=gs://$BUCKET/staging --gcs-log-dir=gs://$BUCKET/logs
