6b5bb7f75e57686af6da62a0e3688a6a4c12e727
Sharif Elgamal <selgamal@google.com>
2016-09-23 14:41:55 -0700

    running tests through gcloud (#35)
    * merging back into fork (#1)
    
    * explicitly specifying a debian mirror for debootstrap
    
    * use the actual correct mirror
    
    * variables are important
    
    * fixing comments
    
    * Switch to envsubst from sed.
    
    * adding tests for debian image
    
    * adding gofmt checking with travis
    
    * updating version of go
    
    * running tests through gcloud
    
    * syntax
    
    * copy comments seems wrong

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM scratch
 ENV DEBIAN_FRONTEND noninteractive
 ENV PATH /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+ENV PORT 8080
 ADD rootfs.tar.xz /
 RUN apt-get -q update && \
     apt-get install --no-install-recommends -y -q ca-certificates && \
---
+++ check-fmt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Checking gofmt..."
+files=$(gofmt -l -s ./tests)
+if [[ $files ]]; then
+    echo "Gofmt errors in files: $files"
+    exit 1
+fi
+echo "Checking go vet..."
+files=$(go vet ./tests)
+if [[ $files ]]; then
+   echo "Go vet errors in files: $files"
+   exit 1
+fi
