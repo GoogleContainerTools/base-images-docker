1e4a297137e29a12d83697e9b3e17e0f0421185b
sharifelgamal <selgamal@google.com>
2016-09-29 15:15:40 -0700

    small change

--- check-fmt.sh
+++ check-fmt.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "Checking gofmt..."
+echo "Checking gofmt"
 files=$(gofmt -l -s ./tests)
 if [[ $files ]]; then
     echo "Gofmt errors in files: $files"
     exit 1
 fi
-echo "Checking go vet..."
+echo "Checking go vet"
 files=$(go vet ./tests)
 if [[ $files ]]; then
    echo "Go vet errors in files: $files"
