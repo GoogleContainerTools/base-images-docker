937e42c0b313aa6886fdf958b430149b80136fe2
sharifelgamal <selgamal@google.com>
2017-03-10 17:26:22 -0800

    adding presubmit test

---
+++ tests/presubmit/presubmit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -ex
+source $KOKORO_GFILE_DIR/common.sh
+cd github/debian-docker
+test_tag="debian-kokoro-presubmit-$KOKORO_BUILD_NUMBER"
+TAG=$test_tag ./build.sh -r gcr.io/gcp-runtimes -v $DEBIAN_SUITE -b gcp-runtimes_cloudbuild
