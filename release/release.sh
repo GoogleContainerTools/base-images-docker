03f09c93330a2beb69d733792ab81675ee2f580b
sharifelgamal <selgamal@google.com>
2017-09-29 15:37:07 -0700

    shellcheck

--- release/release.sh
+++ release/release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-source "$KOKORO_GFILE_DIR/common.sh" #shellcheck source=/dev/null
+source "$KOKORO_GFILE_DIR/common.sh" # shellcheck source=/dev/null
 "$KOKORO_GFILE_DIR/verify-commits.sh"
 cd github/debian-docker
 ./build.sh -r "$DOCKER_NAMESPACE" -v "$DEBIAN_SUITE"
