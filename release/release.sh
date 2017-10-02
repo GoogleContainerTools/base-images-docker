cb86d30a5f5bc1084cdd8c5dc61a909329bfde9e
sharifelgamal <selgamal@google.com>
2017-10-02 11:08:04 -0700

    call script directly instead of cd'ing

--- release/release.sh
+++ release/release.sh
@@ -4,6 +4,5 @@ set -ex
 # shellcheck source=/dev/null
 source "$KOKORO_GFILE_DIR/common.sh"
 "$KOKORO_GFILE_DIR/verify-commits.sh"
-cd github/debian-docker
-./build.sh -r "$DOCKER_NAMESPACE" -v "$DEBIAN_SUITE"
+github/debian-docker/build.sh -r "$DOCKER_NAMESPACE" -v "$DEBIAN_SUITE"
 
