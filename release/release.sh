597b6fe111b0a78a7806e55a0a4a8e0a0531dc06
sharifelgamal <selgamal@google.com>
2017-10-03 14:51:45 -0700

    fixing release script permissions

--- release/release.sh
+++ release/release.sh
@@ -3,6 +3,7 @@ set -ex
 
 # shellcheck source=/dev/null
 source "$KOKORO_GFILE_DIR/common.sh"
-"$KOKORO_GFILE_DIR/verify-commits.sh"
+sudo chmod +x "$KOKORO_GFILE_DIR"/verify-commits.sh
+"$KOKORO_GFILE_DIR"/verify-commits.sh
 github/debian-docker/build.sh -r "$DOCKER_NAMESPACE" -v "$DEBIAN_SUITE"
 
