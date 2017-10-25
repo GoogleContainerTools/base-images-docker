920cf0facbdae311b73cd6818122a254cb665089
sharifelgamal <selgamal@google.com>
2017-10-25 11:15:43 -0700

    installing shellcheck

--- tests/presubmit/presubmit.sh
+++ tests/presubmit/presubmit.sh
@@ -4,6 +4,7 @@ set -ex
 source "$KOKORO_GFILE_DIR/common.sh"
 sudo chmod +x "$KOKORO_GFILE_DIR/verify-commits.sh"
 "$KOKORO_GFILE_DIR"/verify-commits.sh
+sudo apt-get install shellcheck
 
 cd github/debian-docker
 # This is what travis currently does. Let's test what's faster.
