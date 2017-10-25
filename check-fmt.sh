af74b8a8672b0cfc7f2b339bcc6376ca0bf03649
dlorenc <lorenc.d@gmail.com>
2017-10-25 10:46:34 -0700

    Add a git hook to run tests, and instructions. (#104)
    * Add a git hook to run tests, and instructions.
    
    * Buildifier to fix hook.
    
    * Remove redundant shellcheck.

--- check-fmt.sh
+++ check-fmt.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+set -ex
+
 echo "Checking gofmt..."
 files=$(gofmt -l -s ./tests)
 if [[ $files ]]; then
     echo "Gofmt errors in files: $files"
     exit 1
 fi
-echo "Checking go vet..."
-files=$(go vet ./tests)
+
+echo "Checking buildifer..."
+# shellcheck disable=SC2046
+files=$(buildifier -mode=check $(find . -not -path "./vendor/*" -name 'BUILD' -o -name '*.bzl' -type f))
 if [[ $files ]]; then
-   echo "Go vet errors in files: $files"
-   exit 1
+    echo "$files"
+    echo "Run 'buildifier -mode fix \$(find . -name BUILD -o -name '*.bzl' -type f)' to fix formatting"
+    exit 1
 fi
+
+
+echo "Checking shellcheck..."
+find . -name "*.sh" | grep -v "third_party/" | xargs shellcheck
