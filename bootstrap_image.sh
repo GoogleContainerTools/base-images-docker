0af90d1e9f7d81590e608f2463869c5ae4abc264
Tejal Desai <tejaldesai@google.com>
2018-01-23 16:18:44 -0800

    initial commit for bootstrap_image_macro

---
+++ bootstrap_image.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+
+# This script kicks of the bootstrap image macro with the right flags.
+
+while getopts t:g:d:v option
+do
+ case "${option}"
+ in
+ t) TARGET=${OPTARG};;
+ g) GIT_ROOT=${OPTARG};;
+ d) DEBUG="--verbose_failures --sandbox_debug";;
+ esac
+done
+
+# Error out if -t does not exists
+if [ -z "${TARGET}" ];  then
+  USAGE="""
+$(basename "$0") <Mandatory_args> <Optional_args>
+Mandatory Args
+ -t <bootstrap_image_macro>
+Optional Args
+ -g <git_root> Local Git root location
+ -d Add debug options to bazel command
+"""
+  exit 1
+fi
+
+if [ -z "${GIT_ROOT}" ]; then
+  GIT_ROOT=`git rev-parse --show-toplevel`
+fi
+
+echo "Running bazel build ${TARGET}"
+bazel build "${TARGET}" \
+  --action_env=GIT_ROOT=${GIT_ROOT} \
+  --sandbox_writable_path=${GIT_ROOT} \
+  ${DEBUG}
+
+# get rid of running this once we figure out how to make put_status output mandatory in bootstrap_image. 
+echo "Running bazel build ${TARGET}_fetch to make we store the downloaded packages in the store back"
+bazel build "${TARGET}_fetch" \
+  --action_env=GIT_ROOT=${GIT_ROOT} \
+  --sandbox_writable_path=${GIT_ROOT} \
+  ${DEBUG}
+
+echo "Please run git status and commit downloaded packages to the git repository"
---
+++ tests/store/git/git_test.py
@@ -0,0 +1,122 @@
+# Copyright 2017 The Bazel Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for git store"""
+
+from contextlib import contextmanager
+import os
+from shutil import rmtree
+import subprocess
+import tempfile
+import unittest
+from store.git.git import LocalGitStore
+
+class GitStoreTest(unittest.TestCase):
+
+  TEST_STORE = "test_store"
+  TEST_KEY = "test_key"
+  GIT_ROOT = "git_root"
+  STATUS_FILE = ".status"
+
+  def setUp(self):
+    self.tmp_root = tempfile.mkdtemp(suffix="test_root_")
+    self.git_root = os.path.join(self.tmp_root, GitStoreTest.GIT_ROOT)
+    os.mkdir(self.git_root)
+    self.store_location = GitStoreTest.TEST_STORE
+    self.status_file = os.path.join(self.tmp_root, GitStoreTest.STATUS_FILE)
+
+  def tearDown(self):
+    rmtree(self.tmp_root)
+
+  @contextmanager
+  def helper_test(self, suppress_error=True):
+    with LocalGitStore(
+       git_root = self.git_root,
+       store_location = self.store_location,
+       key = GitStoreTest.TEST_KEY,
+       suppress_error = suppress_error,\s
+       status_file = self.status_file) as git_store:
+       yield git_store\s
+
+  def _create_store(self, with_key=False):
+    self.abs_store = os.path.join(self.git_root, self.store_location)
+    os.mkdir(self.abs_store)
+    if with_key:
+      open(os.path.join(self.abs_store, GitStoreTest.TEST_KEY), "w").close()
+
+  def _assert_exit_code(self, expected_exit_code):
+    # Check if the status file has right exit code
+    with open(self.status_file) as sf:
+      exit_code = sf.readlines()[0]
+      self.assertEquals(expected_exit_code, exit_code)
+
+  def testGetIfKeyExists(self):
+    self._create_store(with_key=True)
+    with self.helper_test() as git_store:
+      dest = os.path.join(self.tmp_root, "get_doc")
+      git_store.get(dest)
+    # Check if get document exists
+    self.assertTrue(os.path.exists(dest))
+    # Check if the status file has right exit code
+    self._assert_exit_code('0')
+
+  def testGetIfKeyNotExistsWithSuppress(self):
+    self._create_store()
+    dest = os.path.join(self.tmp_root, "get_doc")
+    with self.helper_test() as git_store:
+      git_store.get(dest)
+    # Make sure dest is not created
+    self.assertFalse(os.path.exists(dest))
+    # Check if the status file has error exit code
+    self._assert_exit_code('1')
+
+  def testGetIfKeyNotExists(self):
+    with self.assertRaises(LocalGitStore.LocalGitStoreError) as e:
+      self._create_store()
+      with self.helper_test(suppress_error=False) as git_store:
+        dest=os.path.join(self.tmp_root, "get_doc")
+        git_store.get(dest)
+
+  def testGetIfStoreNotExists(self):
+    with self.assertRaises(LocalGitStore.LocalGitStoreError) as e:
+      with self.helper_test(suppress_error=False) as git_store:
+        dest=os.path.join(self.tmp_root, "get_doc")
+        git_store.get(dest)
+
+  def _create_src(self):
+    (src_fp, src) = tempfile.mkstemp(dir=self.tmp_root)
+    return src\s
+
+  def testPutIfKeyNotExists(self):
+    self._create_store()
+    with self.helper_test() as git_store:
+      src = self._create_src()
+      git_store.put_if_not_exists(src)
+    # Make sure key exists in the store
+    self.assertTrue(os.path.exists(os.path.join(self.abs_store, GitStoreTest.TEST_KEY)))
+    self._assert_exit_code('0')
+
+  def testPutIfNotExistsWhenKeyExists(self):
+    abs_store = self._create_store(with_key=True)
+    key_file = os.path.join(self.abs_store, GitStoreTest.TEST_KEY)
+    m_time = os.path.getmtime(key_file)
+    with self.helper_test(suppress_error=False) as git_store:
+      src = self._create_src()
+      git_store.put_if_not_exists(src)
+    self.assertEquals(m_time, os.path.getmtime(key_file))
+    self._assert_exit_code('0')
+
+
+if __name__ == '__main__':
+    unittest.main()
