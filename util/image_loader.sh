800efebb5fe379c144a0f1916cba07dd0583db89
Arthur Rabinovich <arthurrab@google.com>
2018-06-12 11:43:32 -0400

    Added tests for the image_loader.sh util

--- util/image_loader.sh
+++ util/image_loader.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-temp=$(docker load --input $1)
+#For overriding loading and using a default value instead
+if [ $1 = "-o" ]
+then
+  temp=$2
+else
+  temp=$(docker load --input $1)
+fi
+
 if [ `echo $temp | cut -d " " -f3` = "ID:" ]
 then # The command outputed the image ID message, so we extract the image ID
   image_name=$(echo $temp | cut -d ":" -f3)
